b2ac289c52030344d55349b1b871c631611958eb
SilviuGitHub <silviugirbacea@gmail.com>
2024-08-09 09:24:45 +0300

    Homework - Java Intro

--- src/main/java/homeworks/java/intro/Calculator.java
+++ src/main/java/homeworks/java/intro/Calculator.java
@@ -1,4 +1,27 @@
 package homeworks.java.intro;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 2;
+        System.out.println("Valori");
+        System.out.println(" x = " + x);
+        System.out.println(" y = " + y);
+
+        System.out.println("Adunare");
+        System.out.println(" x + y = " + (x + y));
+
+        System.out.println("Scadere");
+        System.out.println(" x - y = " + (x - y));
+
+        System.out.println("Inmultire");
+        System.out.println(" x * y = " + (x * y));
+
+        System.out.println("Impartire");
+        System.out.println(" x / y = " + (x / y));
+
+    }
 }
--- src/main/java/homeworks/java/intro/LeapYear.java
+++ src/main/java/homeworks/java/intro/LeapYear.java
@@ -1,4 +1,16 @@
 package homeworks.java.intro;
 
 public class LeapYear {
+    public static void main(String args[]) {
+
+        int month, year;
+        month = 2;
+        year = 1976;
+        if ((month == 2) && ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))))
+            System.out.println("Number of days is 29");
+
+        else if (month == 2)
+            System.out.println("Number of days is 28");
+
+    }
 }

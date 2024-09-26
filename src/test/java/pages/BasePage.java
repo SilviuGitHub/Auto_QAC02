39fab8094f54fbdd55713576d9afcf5528afc86d
SilviuGitHub <silviugirbacea@gmail.com>
2024-09-26 11:47:04 +0300

    Alerts, Modal, Dropdowns, Cookies, Browser Options

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,15 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+
+import static utils.ConstantUtils.SCREENSHOTS_PATH;
 
 public class BasePage {
     protected WebDriver driver;
@@ -27,4 +30,43 @@ public class BasePage {
     public void waitUntilElementVisible(WebElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
     }
+
+    public void waitUntilElementClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public Alert waitUntilAlertIsPresent() {
+        return wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    public String getAlertText(Alert alert) {
+        return alert.getText();
+    }
+
+    public void acceptAlert(Alert alert) {
+        //Press the OK button from alert
+        alert.accept();
+    }
+
+    public void acceptAlert(Alert alert, String response) {
+        //enter response on prompt alert
+        alert.sendKeys(response);
+        //Press the OK button from alert
+        alert.accept();
+    }
+
+    public void cancelAlert(Alert alert) {
+        //Press the OCancel button from alert
+        alert.dismiss();
+    }
+
+    public boolean isAlertClosed() {
+        try {
+            driver.switchTo().alert();
+            return false;
+        } catch (NoAlertPresentException e) {
+            return true;
+        }
+    }
+
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import pages.BasePage;
 import utils.BrowserUtils;
 import utils.ConfigUtils;
 import utils.ConstantUtils;
@@ -10,6 +14,8 @@ import utils.ConstantUtils;
 public class BaseTest {
     protected WebDriver driver;
     protected String baseURL;
+    protected BasePage basePage;
+    protected Alert alert;
 
     public void getBrowser(String browserName) {
         driver = BrowserUtils.getDriver(browserName);
@@ -20,6 +26,17 @@ public class BaseTest {
                 "browser", "chrome");
         System.out.println("Load browser type: " + browserName);
         driver = BrowserUtils.getDriver(browserName);
+        basePage = new BasePage(driver);
+    }
+
+    public void getBrowserWithEnv() {
+        String browserName = ConfigUtils.readGenericElementFromConfig(ConstantUtils.DEFAULT_CONFIG_FILE,
+                "browser", "chrome");
+        String environment = ConfigUtils.readGenericElementFromConfig(ConstantUtils.DEFAULT_CONFIG_FILE,
+                "environment", "local");
+        System.out.println("Load browser type: " + browserName);
+        driver = BrowserUtils.getDriver(browserName, environment);
+        basePage = new BasePage(driver);
     }
 
     public void setUp() {
@@ -50,6 +67,23 @@ public class BaseTest {
 
     public void getBaseURL(String configFileName) {
         baseURL = ConfigUtils.readGenericElementFromConfig(configFileName, "base.url");
+    }
+
+    public void navigateToURL(String path) {
+        System.out.println("Open next url:" + baseURL + path);
+        driver.navigate().to(baseURL + path);
+    }
+
+    public void verifyAlertIsClosed() {
+        Assert.assertTrue(basePage.isAlertClosed());
+        System.out.println("Alert was closed successfully");
+    }
+
+    public void verifyAlertText(String expectedText) {
+        System.out.println("Get alert text");
+        alert = basePage.waitUntilAlertIsPresent();
+        System.out.println(basePage.getAlertText(alert));
+        Assert.assertEquals(basePage.getAlertText(alert), expectedText);
     }
 
 }
